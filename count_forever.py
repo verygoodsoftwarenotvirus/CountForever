322dfc24c69f79f649cad3cae90a7f547602e0b7
LiterallyElvis <literallyelvis@gmail.com>
2014-06-16 08:29:27 -0500

    Cleanup
    removed useless variable and decreased line count

--- count_forever.py
+++ count_forever.py
@@ -1,14 +1,10 @@
 from TwitterAPI import TwitterAPI
 import time
 
-start = time.time()
 limit = 36
 number = 1
 
-api = TwitterAPI('CREDENTIALS',
-                 'GO',
-                 'RIGHT',
-                 'HERE')
+api = TwitterAPI('CREDENTIALS', 'GO', 'RIGHT', 'HERE')
 
 while True:
     time.sleep(limit)
