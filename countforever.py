29978ae760bd3db32a3e70730ec5fab46c209572
Jeffrey D <literallyelvis@gmail.com>
2014-07-15 20:21:05 -0500

    PEP8 compliance.

--- countforever.py
+++ countforever.py
@@ -3,10 +3,12 @@ from num2words import num2words
 import time
 
 limit, number = 36, 1
+phrase = "...ah ah ah!"
 
 api = TwitterAPI('CREDS', 'GO', 'RIGHT', 'HERE')
 
 while True:
-    api.request('statuses/update', {'status':num2words(number).replace(" and", "") + '...ah ah ah!'})
+    api.request('statuses/update',
+                {'status': num2words(number).replace(" and", "") + phrase})
     number += 1
     time.sleep(limit)
