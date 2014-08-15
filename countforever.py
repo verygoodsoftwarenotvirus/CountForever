c5ecb8bd8f549137c0552a3daf54aa7016814c0c
Jeffrey D. <literallyelvis@gmail.com>
2014-08-15 06:06:29 -0500

    Update countforever.py

--- countforever.py
+++ countforever.py
@@ -3,12 +3,11 @@ from num2words import num2words
 import time
 
 limit, number = 36, 1
-phrase = "...ah ah ah!"
 
 api = TwitterAPI('CREDS', 'GO', 'RIGHT', 'HERE')
 
 while True:
-    api.request('statuses/update',
-                {'status': num2words(number).replace(" and", "") + phrase})
+    tweet = num2words(number).replace(" and", "") + "...ah ah ah!"}
+    api.request('statuses/update', {'status': tweet})
     number += 1
     time.sleep(limit)
