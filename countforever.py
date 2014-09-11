03b9d6976bcefadc976427118fd7efc69ddbd96c
Jeffrey D <literallyelvis@gmail.com>
2014-09-11 01:52:48 -0500

    Fixed Syntax error.

--- countforever.py
+++ countforever.py
@@ -7,7 +7,7 @@ limit, number = 36, 1
 api = TwitterAPI('CREDS', 'GO', 'RIGHT', 'HERE')
 
 while True:
-    tweet = num2words(number).replace(" and", "") + "...ah ah ah!"}
+    tweet = num2words(number).replace(" and", "") + "...ah ah ah!"
     api.request('statuses/update', {'status': tweet})
     number += 1
     time.sleep(limit)
