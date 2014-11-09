56a94224cd4fc71c189bf87229514ff69173e758
Jeffrey D. <literallyelvis@gmail.com>
2014-11-09 04:02:54 -0600

    Spacing is hard

--- countforever.py
+++ countforever.py
@@ -8,8 +8,8 @@ number = 1
 api = TwitterAPI('CREDS', 'GO', 'RIGHT', 'HERE')
 
 while True:
-	try:
-		tweet = num2words(number).replace(" and", "") + "...ah ah ah!"
+    try:
+    	tweet = num2words(number).replace(" and", "") + "...ah ah ah!"
     	api.request('statuses/update', {'status': tweet})
     	number += 1
     	time.sleep(limit)
