7028441eb83db081608c9d0e3d7038d8261a22e2
Jeffrey D <literallyelvis@gmail.com>
2014-11-09 04:01:30 -0600

    Finally some error handling.

--- countforever.py
+++ countforever.py
@@ -8,7 +8,10 @@ number = 1
 api = TwitterAPI('CREDS', 'GO', 'RIGHT', 'HERE')
 
 while True:
-    tweet = num2words(number).replace(" and", "") + "...ah ah ah!"
-    api.request('statuses/update', {'status': tweet})
-    number += 1
-    time.sleep(limit)
+	try:
+		tweet = num2words(number).replace(" and", "") + "...ah ah ah!"
+    	api.request('statuses/update', {'status': tweet})
+    	number += 1
+    	time.sleep(limit)
+    except requests.exceptions.Timeout:
+    	time.sleep(1800)  # half an hour in seconds
