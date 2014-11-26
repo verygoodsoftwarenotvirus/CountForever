f2daaaf027719e1926c3e63f8e663417b6ece085
Jeffrey D. <literallyelvis@gmail.com>
2014-11-26 11:56:15 -0600

    Magic numbers are bad

--- countforever.py
+++ countforever.py
@@ -3,7 +3,8 @@ from num2words import num2words
 import requests
 import time
 
-limit = 36
+limit = 36  # 100 tweets per hour
+ten_minutes = 600  # in seconds
 number = 1
 
 api = TwitterAPI('CREDS', 'GO', 'RIGHT', 'HERE')
@@ -15,4 +16,4 @@ while True:
     	number += 1
     	time.sleep(limit)
     except requests.exceptions.Timeout:
-    	time.sleep(600)  # ten minutes in seconds
+    	time.sleep(ten_minutes)
