adbc180655e5e3a77825b6a3fcf946f4e92f6e70
Jeffrey D. <LiterallyElvis@users.noreply.github.com>
2015-05-13 02:35:41 -0500

    Update exception
    A specific exception means that when requests encounters an exception that is not specifically the one you have handled, but is rather another very very similar one, everything dies and I get inquisitive text messages from my Dad about why the count isn't tweeting.

--- countforever.py
+++ countforever.py
@@ -1,7 +1,6 @@
 from TwitterAPI import TwitterAPI
 from num2words import num2words
 from time import sleep
-from requests.exceptions import Timeout
 
 limit = 36  # 100 tweets per hour
 ten_minutes = 600  # in seconds
@@ -15,7 +14,7 @@ while True:
         api.request('statuses/update', {'status': tweet})
         number += 1
         sleep(limit)
-    except Timeout:
+    except:
         """
         Sometimes DigitalOcean works late hours and spills coffee on its pants.
         When this happens, it needs about ten minutes to go home and change.
